1ce0918d0a729ec619d9d1b20bff34f5b01bf18f
andre macedo <andre@macedo>
2020-08-24 01:53:44 -0300

    Implementação do método de coletar notícias
    implementado método parta clicar no botão de carrega mais e verificar se
    as novas noticias foram carregadas.
    
    No momento não funciona, devido a manipulação necessária para lidar com
    o js event conectado a div do botão

--- src/main/java/br/com/knewin/teste/model/HomePage.java
+++ src/main/java/br/com/knewin/teste/model/HomePage.java
@@ -1,13 +1,19 @@
 package br.com.knewin.teste.model;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
+	
+	public static final int QUANTITY_OF_LOADED_NEWS = 10;
+	private static final String LAST_LOADED_NEWS_XPATH = "/html/body/div[4]/div[4]/div/div/div";
 	
 	private By lastNewsBy = By.xpath("//div[@id='infiniteScroll']//div[contains(@class, 'img-container')]/a");
 	private By loadMoreButtonBy = By.xpath("//div[@id='infinite-handle']");
@@ -38,7 +44,15 @@ public class HomePage {
 		this.lastNewsUrls = lastNewsUrls;
 	}
 
-	public void setLoadMoreButtonBy(By loadMoreButtonBy) {
-		this.loadMoreButtonBy = loadMoreButtonBy;
+	public void loadMoreNews() {
+		this.driver.manage().window().setPosition(this.getLoadMoreButton().getLocation());
+		this.driver.manage().window().fullscreen();
+		
+		int quantityOfLoadedNews = this.getLastNewsUrls().size() + QUANTITY_OF_LOADED_NEWS;
+		By lastLoadedNewsBy = By.xpath(LAST_LOADED_NEWS_XPATH + "[" + quantityOfLoadedNews + "]");
+		
+		this.getLoadMoreButton().click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10).getSeconds());
+		wait.until(ExpectedConditions.presenceOfElementLocated(lastLoadedNewsBy));
 	}
 }
